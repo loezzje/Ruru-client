64541b1db3557b1a35dae83f4655c999e9d8074e
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-16 17:57:33 +0200

    add font-awesome to index.sass

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-<<<<<<< Updated upstream
         <Routes />
         <Footer />
-||||||| merged common ancestors
-=======
-        <Footer />
->>>>>>> Stashed changes
       </div>
     );
   }

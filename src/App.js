84ec391abbaf74570fc5381792a46868723ad8ce
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-16 17:24:46 +0200

    Add font-awesome to index.css and add footer to app for now

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Routes from './routes'
 import './App.css';
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {

a6ed9c3492c6be3521a839244a7e510a89753358
almozaz <alacruzmozaz@gmail.com>
2017-10-17 12:25:20 +0200

    fetch faq

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import menuShow from './menu.js'
 import organizations from './organizations'
 import categories from './categories'
 import ruru from './ruru'
+import faq from './faq'
 
 export default {
   menuShow,
   organizations,
   categories,
   ruru,
+  faq,
 }

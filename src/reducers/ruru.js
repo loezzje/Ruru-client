4617365561d8bae40195b2aaa685e8f639b85753
almozaz <alacruzmozaz@gmail.com>
2017-10-17 12:02:32 +0200

    update ruru data

--- src/reducers/ruru.js
+++ src/reducers/ruru.js
@@ -1,8 +1,12 @@
 import { FETCH_RURU  } from '../actions/ruru/fetch'
+import { UPDATE_RURU  } from '../actions/ruru/update'
 
 export default (state = [], {type, payload} =  {}) => {
   switch (type)  {
     case FETCH_RURU :
+      return [].concat(payload)
+
+    case UPDATE_RURU :
       return [].concat(payload)
 
     default :

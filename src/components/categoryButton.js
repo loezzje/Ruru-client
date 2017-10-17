c0212f80ef572bf6a4345428e95bbf1838bfde58
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-17 18:01:09 +0200

    Button has icon and is centered

--- src/components/categoryButton.js
+++ src/components/categoryButton.js
@@ -1,13 +1,17 @@
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
+import './categoryButton.css'
 
 export class categoryButton extends PureComponent {
   render () {
-    const { _id, name } = this.props
+    const { _id, icon, name } = this.props
 
     return(
       <article className='categoryButton'>
-        <Link to={`/categories/${_id}`}>{ name }</Link>
+        <Link to={`/categories/${_id}`}>
+          <div className='icon'><img src={ icon } alt='icon' /></div>
+          <span className='name'>{ name }</span>
+        </Link>
       </article>
     )
   }

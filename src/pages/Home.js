1994d499fe51d390bf79ee7d38a7f130bc34250a
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-17 14:41:07 +0200

    Add buttonfile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import categoryButton from '../components/categoryButton'
 
 export class Home extends PureComponent {
 
@@ -7,9 +8,11 @@ export class Home extends PureComponent {
     return(
       <div className='homepage'>
         <header>
-          <h1>Homepage</h1>
+          <h3>Welcome to RuRu</h3>
+          <h5>An information handbook for newcomers to the Netherlands</h5>
         </header>
         <main>
+          <categoryButton />
         </main>
       </div>
     )

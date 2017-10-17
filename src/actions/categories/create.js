dac5c79d3f0a6cf90885cb4d0878d313f03e183f
almozaz <alacruzmozaz@gmail.com>
2017-10-17 10:11:50 +0200

    create category

---
+++ src/actions/categories/create.js
@@ -0,0 +1,24 @@
+import API from '../../api'
+
+export const CREATE_CATEGORY = 'CREATE_CATEGORY'
+
+const api = new API()
+
+export default (category) => {
+  return (dispatch) => {
+
+    api.app.authenticate()
+      .then(() => {
+        const backend = api.service('categories')
+        backend.create(category)
+          .then((result) => {
+            dispatch({
+              type: CREATE_CATEGORY,
+              payload: result
+            })
+          })
+          .catch((error) => {
+          })
+      })
+  }
+}
--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,9 +1,13 @@
 import { FETCH_CATEGORIES } from '../actions/categories/fetch'
+import { CREATE_CATEGORY } from '../actions/categories/create'
 
 export default (state = [], { type, payload } = {}) => {
   switch (type) {
     case FETCH_CATEGORIES:
       return [].concat(payload)
+
+    case CREATE_CATEGORY:
+      return state.concat(payload)
 
     default:
       return state

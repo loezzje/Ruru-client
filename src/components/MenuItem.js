82a9349b2aa613dd8536fb62185555a59aa31469
TristanKwant <tristankwant@gmail.com>
2017-10-17 11:58:23 +0200

    add submenu toggle

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,22 +2,65 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import './MenuItem.css'
+import DownIcon from 'react-icons/lib/fa/angle-down'
+
 
 class MenuItem extends PureComponent {
+  constructor(props) {
+    super()
 
+    const {  } = props
+
+    this.state = {
+      submenu: false
+    }
+  }
+  toggleSubMenu(){
+    const { submenu } = this.state
+
+    if (submenu) {
+      this.setState({
+        submenu: false
+      })
+    } else {
+      this.setState({
+        submenu: true
+      })
+    }
+
+  }
+
+  renderSubMenu(){
+    const { submenu } = this.state
+
+    if (submenu) {
+      return <div> submenu</div>
+    } else {
+      return null
+    }
+  }
 
   render() {
     const { name } = this.props
     return (
       <li className="menu-item">
-        <h1>
-          {name}
-        </h1>
+        <div onClick={this.toggleSubMenu.bind(this)}>
+          <h1>
+            {name}
+          </h1>
+          <DownIcon className="down-arrow" />
+        </div>
+        <nav>
+          <ul>
+            {this.renderSubMenu()}
+          </ul>
+        </nav>
+
       </li>
 
     )
   }
 }
-const mapStateToProps = ({ categories }) => ({ ...categories })
+const mapStateToProps = ({ categories, organizations }) => ({ organizations, ...categories })
 
 export default connect(mapStateToProps)(MenuItem)

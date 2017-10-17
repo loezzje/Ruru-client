568e74e779444e5d64d500b54795484cab93ee07
loezzje <m.demmendaal@hotmail.com>
2017-10-17 12:02:39 +0200

    actions update organization

---
+++ src/actions/organizations/patch.js
@@ -0,0 +1,24 @@
+import API from '../../api'
+
+export const PATCH_ORGANIZATION = 'PATCH_ORGANIZATION'
+
+const api = new API()
+
+export default (id, newdata) => {
+  return (dispatch) => {
+
+    api.authenticate()
+    .then(() => {
+    const backend = api.service('organizations')
+    backend.update(id, {$set:newdata})
+    .then((result) => {
+      dispatch({
+        type: PATCH_ORGANIZATION,
+        payload: result
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+)}}
--- src/reducers/organizations.js
+++ src/reducers/organizations.js
@@ -1,6 +1,7 @@
 import { FETCH_ORGANIZATIONS } from '../actions/organizations/fetch'
 import { CREATE_ORGANIZATION } from '../actions/organizations/create'
 import { DELETE_ORGANIZATION } from '../actions/organizations/delete'
+import { PATCH_ORGANIZATION } from '../actions/organizations/patch'
 
 export default (state = [], {type, payload} =  {}) => {
   switch (type)  {
@@ -12,6 +13,10 @@ export default (state = [], {type, payload} =  {}) => {
 
     case DELETE_ORGANIZATION :
       return state.filter(organization => organization._id !== payload._id)
+
+    case PATCH_ORGANIZATION :
+      return state.filter(function(organization){ if (organization._id === payload._id) return payload;
+        return organization })
 
   default :
     return state

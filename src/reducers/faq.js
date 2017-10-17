afc41052cb7e685189fa3b14edfaddee31ea477b
almozaz <alacruzmozaz@gmail.com>
2017-10-17 13:27:29 +0200

    create faq

--- src/reducers/faq.js
+++ src/reducers/faq.js
@@ -1,4 +1,5 @@
 import { FETCH_FAQ } from '../actions/faq/fetch'
+import { CREATE_FAQ } from '../actions/faq/create'
 import { UPDATE_FAQ } from '../actions/faq/update'
 import { DELETE_FAQ } from '../actions/faq/delete'
 
@@ -6,6 +7,9 @@ export default (state = [], {type, payload} =  {}) => {
   switch (type)  {
     case FETCH_FAQ :
       return [].concat(payload)
+
+    case CREATE_FAQ:
+      return state.concat(payload)
 
     case UPDATE_FAQ :
       return state.map(function(question){

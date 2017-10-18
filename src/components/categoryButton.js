4b4f87c9d8995e3a723b9db7c57cd227f8c46cec
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-18 10:08:36 +0200

    Added hardcoded description for now

--- src/components/categoryButton.js
+++ src/components/categoryButton.js
@@ -4,13 +4,14 @@ import './categoryButton.css'
 
 export class categoryButton extends PureComponent {
   render () {
-    const { _id, icon, name } = this.props
+    const { _id, icon, name, description } = this.props
 
     return(
       <article className='categoryButton'>
         <Link to={`/categories/${_id}`}>
           <div className='icon'><img src={ icon } alt='icon' /></div>
           <span className='name'>{ name }</span>
+          <div className='description'>{ description }</div>
         </Link>
       </article>
     )

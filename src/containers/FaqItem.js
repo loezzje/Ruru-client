d07c5134a5c6cb8f9e230af4e2fe27b7561d0c18
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-18 15:02:14 +0200

    Some basic faqitem css

--- src/containers/FaqItem.js
+++ src/containers/FaqItem.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react'
+import './FaqItem.css'
 
 export class FaqItem extends PureComponent {
   render () {
     const { question, answer } = this.props
 
     return(
-      <article className='FaqItem'>
+      <article className='faqitem'>
           <div className='question'><p>{ question }</p></div>
           <div className='answer'><p>{ answer }</p></div>
       </article>

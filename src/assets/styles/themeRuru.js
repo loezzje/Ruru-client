8df816a475a6cccb699971658223e37a87b242fe
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-18 12:06:28 +0200

    put colours in sassfile in componentfolder

--- src/assets/styles/themeRuru.js
+++
@@ -1,9 +0,0 @@
-//Ruru colours
-export const brown        = '#987314'
-export const green        = '#398b7d'
-export const lilac        = '#a16a81'
-export const pink         = '#e4676e'
-export const orange       = '#ff5840'
-export const brownDark    = '#413c31'
-export const white        = '#ffffff'
-export const black        = '#000000'

edbf82ee46e38e1d60cc554588c02262eddb1d56
atomicsaiko <atomicsaiko@gmail.com>
2017-10-18 12:17:03 +0200

    Add action creator and reducer to get a single category from API

--- src/pages/TopicCardList.js
+++ src/pages/TopicCardList.js
@@ -2,12 +2,14 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import fetchOrganizations from '../actions/organizations/fetch'
+import getCategory from '../actions/categories/get'
 import TopicCard from '../components/TopicCard'
 
 class TopicCardList extends PureComponent {
   componentWillMount() {
-    const { fetchOrganizations } = this.props
+    const { fetchOrganizations, getCategory } = this.props
     fetchOrganizations()
+    getCategory(this.props.match.params.categorieId)
   }
 
   /* It should be read from the 'tagline' property in organizations schema */
@@ -22,6 +24,8 @@ class TopicCardList extends PureComponent {
   }
 
   render() {
+    // console.log("Match params: ", this.props.match.params)
+    console.log('Show single category: ', this.props.category)
     return (
       <div>
         Category full width tile here.
@@ -32,8 +36,25 @@ class TopicCardList extends PureComponent {
   }
 }
 
-const mapStateToProps = ({ organizations }) => ({
-  organizations
+// const mapStateToProps = ({ organizations, categories }, { match }) => ({
+//
+//   const category = categories.reduce((prev, next) => {
+//     if (next._id === match.params.categorieId) {
+//       return next
+//     }
+//     return prev
+//   }, {})
+//
+//   return {
+//     category,
+//     ...category,
+//     organizations
+//   }
+// })
+
+const mapStateToProps = ({ organizations, category }) => ({
+  organizations,
+  category
 })
 
-export default connect(mapStateToProps, {fetchOrganizations})(TopicCardList)
+export default connect(mapStateToProps, {fetchOrganizations, getCategory})(TopicCardList)

49515aa584e8c19ef63a1e4446d59eb717a80bbb
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-20 14:47:55 +0200

    Useless contactform in containers. jeee

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,20 @@
 import React, { PureComponent } from 'react'
 // import Mailto from 'react-mailto'
+import ContactForm from '../containers/ContactForm'
 import './Contact.css'
 
 export class Contact extends PureComponent {
 
   render() {
     return(
-      <div className='contactpage'>
+      <div className='contactpage main-container'>
         <header>
           <h3>Contact</h3>
         </header>
 
         <main>
-          <p>Keen to help us grow? Send us an </p>
+          <p>Keen to help us grow? Send us an e-mail:</p>
+          <ContactForm />
         </main>
       </div>
     )

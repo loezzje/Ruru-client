62802913ef02a630e2a24ef4cbe971c2b7a38622
atomicsaiko <atomicsaiko@gmail.com>
2017-10-21 21:39:19 +0200

    Refactor Menu component and add Prop type checking

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,42 +5,39 @@ import './Menu.css'
 import MenuItem from './MenuItem.js'
 import { CSSTransitionGroup } from 'react-transition-group'
 
-
-
-
 class Menu extends PureComponent {
-  renderMenu(categorie, index){
-  return  <MenuItem key={index} {...categorie}/>
 
+  renderMenu(categorie, index) {
+    return <MenuItem key={index} {...categorie} />
   }
-
-
 
   render() {
     const { menuShow } = this.props
-     if (!menuShow) return null
+    if (!menuShow) return null
 
     return (
       <div id="menu" className={menuShow ? "openMenu": ""}>
         <ul>
           <CSSTransitionGroup
-          transitionAppear={true}
-          transitionName="example"
-          transitionEnter={true}
-          transitionAppearTimeout={500}
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}>
-          { this.props.categories.map(this.renderMenu.bind(this))}
-        </CSSTransitionGroup>
-
+            transitionAppear={true}
+            transitionName="example"
+            transitionEnter={true}
+            transitionAppearTimeout={500}
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}>
+            { this.props.categories.map(this.renderMenu.bind(this))}
+          </CSSTransitionGroup>
         </ul>
       </div>
-
-
-
     )
   }
 }
+
 const mapStateToProps = ({ menuShow, categories }) => ({ menuShow, categories })
 
 export default connect(mapStateToProps)(Menu)
+
+Menu.propTypes = {
+  menuShow: PropTypes.bool.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.object)
+}

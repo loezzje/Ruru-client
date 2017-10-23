af27bb23742c2a14649178b66ba8cac24865025d
EAPCMC <E.A.PEENE@GMAIL.com>
2017-10-23 14:19:34 +0200

    single event css basic

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,20 +1,25 @@
 import React, { PureComponent } from 'react'
+import Eye from '../assets/logos/RURU_eye.png'
+import './Event.css'
+
+function truncate(str, no_words) {
+    return str.split(" ").splice(0,no_words).join(" ");
+}
 
 class Event extends PureComponent {
   render() {
     const { cover, name, description, place, start_time } = this.props
 
-
     return (
-      <article className="event-item">
-        <div className='small-cover'>
+      <article className="eventitem">
+        <div className='smallcover'>
           <img src={ cover.source } alt='fbcover' />
         </div>
           <h3 className="event-title">{ name }</h3>
           <p>{ start_time === undefined ? "no date" : start_time }</p>
         <div className="event-details">
-          <p>{ place === undefined ? "no location set" : place.name }</p>
-          <p>{ description === undefined ? "no description" : description }</p>
+          <span className="eyecon"><img src={ Eye } alt="location-icon" />{ place === undefined ? "no location set" : place.name }</span>
+          <p>{ description === undefined ? "no description" : truncate(description, 50) }</p>
         </div>
       </article>
     )

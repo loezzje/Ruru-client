355d94464460532f1d1c46116229fa1c86f3f143
atomicsaiko <atomicsaiko@gmail.com>
2017-10-23 13:54:11 +0200

    Implement delete logic for Category, Organization and FAQ

--- src/components/ListItemAdmin.js
+++ src/components/ListItemAdmin.js
@@ -1,17 +1,44 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import deleteCategory from '../actions/categories/delete'
+import deleteOrganization from '../actions/organizations/delete'
+import deleteFaq from '../actions/categories/delete'
 import { Link } from 'react-router-dom'
 import './ListItemAdmin.css'
 
 class ListItemAdmin extends PureComponent {
 
+  deleteEntry(event) {
+    event.preventDefault()
+
+    const { deleteCategory, deleteOrganization, deleteFaq, name } = this.props
+
+    var parseURI = this.props.path.split('/')
+    var ObjectId = parseURI[2] // Make sure order of URI in routes has the ObjectId after /admin/
+
+    if (parseURI.includes('create-category')) {
+      if (window.confirm(`Are you sure you want to delete Category: "${name}"`))
+      deleteCategory(ObjectId)
+    } else if (parseURI.includes('create-organization')) {
+      if (window.confirm(`Are you sure you want to delete Organization: "${name}"`))
+      deleteOrganization(ObjectId)
+    } else {
+      if (window.confirm(`Are you sure you want to delete Faq: "${name}"`))
+      deleteFaq(ObjectId)
+    }
+  }
+
   render() {
     return (
       <li className="listitem-admin">
         <span>{ this.props.name }</span>
+        <Link to={{ pathname: this.props.path }} onClick={this.deleteEntry.bind(this)}>Delete</Link>
         <Link to={{ pathname: this.props.path }}>Edit</Link>
       </li>
     )
   }
 }
 
-export default ListItemAdmin
+const mapDispatchToProps = { deleteCategory, deleteOrganization, deleteFaq }
+
+export default connect(null, mapDispatchToProps)(ListItemAdmin)

cb8fb9cf054e0882d75c61c074cf3cb2eea47948
atomicsaiko <atomicsaiko@gmail.com>
2017-10-24 10:33:35 +0200

    Update About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,17 +5,28 @@ import './About.css'
 
 export class About extends PureComponent {
   render() {
-    const { about } = this.props
+    // const { about } = this.props
 
     return(
       <div className='aboutpage main-container'>
         <header>
-        <img className="big-logo" src={ Logo } alt="RURU icon"/>
+          <img className="big-logo" src={ Logo } alt="RURU icon"/>
           <h3>ABOUT RURU</h3>
         </header>
 
         <main>
-          <p>{ about }</p>
+          {/* <p>{ about }</p> */}
+          <p>
+            RURU is a community-driven, online information platform for newcomers to the Netherlands. Centralising the many services, organisations, groups and abundance of information into ONE reliable, up to date, easy to use platform.
+          </p>
+
+          <p>
+            RURU works with newcomers to provide essential information and tips on their new home, including LIFE, HEALTH, EDUCATION, RIGHTS & LAWS, JOBS, HOUSING and EVENTS.
+          </p>
+
+          <p>
+            Currently there is a lot available to newcomers, however finding what is out there is difficult and time consuming. RURU makes sense of it all and allows newcomers to access what already exists fast and easy.
+          </p>
         </main>
       </div>
     )
